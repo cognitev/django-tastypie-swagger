6900f2a7e85e39a02e91012b6955a88a5a71c462
AlSayed Gamal <gamal@cognitev.com>
2020-07-16 12:53:18 +0200

    Upgrade django2.2 (#1)
    Co-authored-by: Alaa <alaanourali@gmail.com>

--- tastypie_swagger/urls.py
+++ tastypie_swagger/urls.py
@@ -1,5 +1,14 @@
 from .views import SwaggerView, ResourcesView, SchemaView
 try:
+    from django.urls import include, re_path
+
+    urlpatterns = [
+        re_path(r'^$', SwaggerView.as_view(), name='index'),
+        re_path(r'^resources/$', ResourcesView.as_view(), name='resources'),
+        re_path(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
+        re_path(r'^schema/$', SchemaView.as_view(), name='schema'),
+    ]
+except ImportError:
     from django.conf.urls import include, url
 
     urlpatterns = [
@@ -8,13 +17,4 @@ try:
         url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
         url(r'^schema/$', SchemaView.as_view(), name='schema'),
     ]
-except ImportError:
-    from django.conf.urls.defaults import patterns, include, url
-
-    urlpatterns = patterns('',
-        url(r'^$', SwaggerView.as_view(), name='index'),
-        url(r'^resources/$', ResourcesView.as_view(), name='resources'),
-        url(r'^schema/(?P<resource>\S+)$', SchemaView.as_view()),
-        url(r'^schema/$', SchemaView.as_view(), name='schema'),
-    )
 
